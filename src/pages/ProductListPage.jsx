c9d45bf4ebf3af9ad0a62a4b3d79425ed2546ef9
Noelia <noeliairanzoribera@gmail.com>
2023-12-18 13:21:53 +0100

    iteration 1

--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import classes from "../styles/listpage.module.css"
 
 
 function ProductListPage() {
@@ -6,12 +7,31 @@ function ProductListPage() {
   // but you should use it to store the response from the Fake Store API (the list of products).
   const [products, setProducts] = useState([]);
 
-  // To fetch the list of products, set up an effect with the `useEffect` hook:
+  
+  //the 'fetch' function sends a network request to the URL and returns a promise.
+  //when the promise is resolved, it passes a response object to te first then method.
+  //result.json() allows us to parse the response as a JSON 
+  //console.log of the result. 
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+            .then(result => result.json())
+            .then(data => setProducts(data))
+            .catch(err => console.log(err));
+  }, []);
 
 
   return (
-    <div className="ProductListPage">
-      {/* Render list of products here */}
+    <div className={classes.mainCtn}>
+      {products.map(product => (
+        <div key={product.id} className={classes.productCtn}>
+          <div className={classes.item1}><img src={product.image} alt={product.title} /></div>
+          <h2 className={classes.item2}>{product.title}</h2>
+          <p className={classes.item3}>{product.category}</p>
+          <p className={classes.item4}>${product.price}</p>
+          <p className={classes.item5}>{product.description}</p>
+        </div>
+      ))}
     </div>
   );
 }
