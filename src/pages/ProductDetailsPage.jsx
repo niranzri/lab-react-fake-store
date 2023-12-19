a772b26bc59c9ae4a4ed094a321bc8fa5e7b3f31
Noelia <noeliairanzoribera@gmail.com>
2023-12-19 10:15:10 +0100

    iteration 2 and 3 added

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,23 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useParams} from "react-router-dom"
+import classes from "../styles/listpage.module.css"
 
 
 function ProductDetailsPage() {
+
+  const [productDetails, setProductDetails] = useState({});
+
   // The state variable `product` is currently an empty object {},
   // but you should use it to store the response from the Fake Store API (the product details).
-  const [product, setProduct] = useState({});
-
-
-  // The `productId` coming from the URL parameter is available in the URL path.
-  // You can access it with the `useParams` hook from react-router-dom.
-
 
   // To fetch the product details, set up an effect with the `useEffect` hook:
+  // The `productId` coming from the URL parameter is available in the URL path.
+  // You can access it with the `useParams` hook from react-router-dom.
+  const { productId } = useParams()
 
+  useEffect(() => {
+    // Fetch individual product details based on productId
+    fetch(`https://fakestoreapi.com/products/${productId}`)
+      .then(result => result.json())
+      .then(productData => setProductDetails(productData))
+      .catch(error => console.log(error));
+  }, [productId]);
 
 
   return (
-    <div className="ProductDetailsPage">
-    {/* Render product details here */}
+    <div className="mainCtn">
+        <div key={productDetails.id} className={classes.productCtn}>
+          <div className={classes.item1}><img src={productDetails.image} alt={productDetails.title} /></div>
+          <h2 className={classes.item2}>{productDetails.title}</h2>
+          <p className={classes.item3}>{productDetails.category}</p>
+          <p className={classes.item4}>${productDetails.price}</p>
+          <p className={classes.item5}>{productDetails.description}</p>
+        </div>
     </div>
   );
 }
--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,38 +1,44 @@
 import { useState, useEffect } from "react";
 import classes from "../styles/listpage.module.css"
-
+import ProductDetailsPage from "./ProductDetailsPage";
+import { Link } from "react-router-dom";
+import * as React from "react";
 
 function ProductListPage() {
   // The state variable `products` is currently an empty array [], 
   // but you should use it to store the response from the Fake Store API (the list of products).
   const [products, setProducts] = useState([]);
-
   
   //the 'fetch' function sends a network request to the URL and returns a promise.
   //when the promise is resolved, it passes a response object to te first then method.
   //result.json() allows us to parse the response as a JSON 
-  //console.log of the result. 
-
   useEffect(() => {
     fetch('https://fakestoreapi.com/products')
             .then(result => result.json())
-            .then(data => setProducts(data))
+            .then(data =>  setProducts(data))
             .catch(err => console.log(err));
   }, []);
+
+  //console.log of the result. 
+  useEffect (() => {
+    console.log(products)
+  }, [products]);
 
 
   return (
     <div className={classes.mainCtn}>
-      {products.map(product => (
-        <div key={product.id} className={classes.productCtn}>
-          <div className={classes.item1}><img src={product.image} alt={product.title} /></div>
-          <h2 className={classes.item2}>{product.title}</h2>
-          <p className={classes.item3}>{product.category}</p>
-          <p className={classes.item4}>${product.price}</p>
-          <p className={classes.item5}>{product.description}</p>
-        </div>
+      {products?.map(product => (
+        <Link key={product.id} to={`/product/details/${product.id}`}>
+          <div className={classes.productCtn}>
+            <div className={classes.item1}><img src={product.image} alt={product.title} /></div>
+            <h2 className={classes.item2}>{product.title}</h2>
+            <p className={classes.item3}>{product.category}</p>
+            <p className={classes.item4}>${product.price}</p>
+            <p className={classes.item5}>{product.description}</p>
+          </div>
+        </Link>
       ))}
-    </div>
+    </div> 
   );
 }
 
